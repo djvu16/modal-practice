a6f938a667074d3bc28abcab32457b648c0ed41a
dtindy <dy83421@gmail.com>
2021-08-10 23:20:10 +0530

    use of React Fragment to avoid div soup and use of react-dom createPortal method for cleaner html code

--- src/components/UI/error-model/error-model.component.js
+++ src/components/UI/error-model/error-model.component.js
@@ -1,24 +1,43 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Card from "../card/card";
 import Button from "../button/button";
 
 import "./error-model.styles.css";
+
+const Backdrop = props =>{
+    return <div className="backdrop" onClick={props.onConfirm} />;
+}
+
+const ModalOverlay = props => {
+    return (<Card className="modal">
+    <header>
+        <h2 className="header">{props.title}</h2>
+    </header>
+    <div className="content">
+        <p>{props.message}</p>
+    </div>
+    <footer className="actions">
+        <Button onClick={props.onConfirm}>Okay</Button>
+    </footer>
+    </Card>)
+}
+
 const ErrorModel = props => {
     return (
-        <div>
-        <div className="backdrop" onClick={props.onConfirm} />
-        <Card className="modal">
-        <header>
-            <h2 className="header">{props.title}</h2>
-        </header>
-        <div className="content">
-            <p>{props.message}</p>
-        </div>
-        <footer className="actions">
-            <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-        </Card>
-        </div>
+        <React.Fragment>
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />,document.getElementById("backdrop-root"))
+            }
+            {
+                ReactDOM.createPortal(
+                    <ModalOverlay 
+                        title={props.title}
+                        message={props.message}
+                        onConfirm={props.onConfirm}
+                    />,
+                    document.getElementById('overlay-root'))
+            }
+        </React.Fragment>
     )
 };
 
